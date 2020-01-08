49793cc338d323746b3bbe3c25f5631150e5adff
João Soares <jsoaresgeral@gmail.com>
2020-01-08 12:21:02 +0000

    Add command line prompt and reader (Scanner) for fetching
    point coordinates from user.
    
    Signed-off-by: João Soares <jsoaresgeral@gmail.com>

--- src/io/jaisoares/geo/Main.java
+++ src/io/jaisoares/geo/Main.java
@@ -1,10 +1,27 @@
 package io.jaisoares.geo;
 
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args) {
-		Point p = new Point(2, 3);
-		Point q = new Point(3, 5);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter coordinate x for point P");
+		int pX = sc.nextInt();
+		System.out.println("Enter coordinate y for point P");
+		int pY = sc.nextInt();
+
+		System.out.println("Enter coordinate x for point Q");
+		int qX = sc.nextInt();
+		System.out.println("Enter coordinate y for point Q");
+		int qY = sc.nextInt();
+
+		// Close your scanner to avoid memory leaks and free resources
+		sc.close();
+
+		Point p = new Point(pX, pY);
+		Point q = new Point(qX, qY);
 
 		Point r = p.add(q);
 		Point s = p.subtract(q);
