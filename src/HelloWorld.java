48b4feeefa1e1b739a79497603b7096bb48436d6
João Soares <jsoaresgeral@gmail.com>
2020-01-08 01:21:58 +0000

    Add run arguments and conditionally greet the passed arg person
    Signed-off-by: João Soares <jsoaresgeral@gmail.com>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,13 @@
+import java.lang.reflect.Array;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		if(Array.getLength(args) > 0) {
+			System.out.println("Hello " + args[0] + " and the world!");
+		} else {
+			System.out.println("Hello World!");
+		}
 	}
 
 }
