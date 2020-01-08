8996a3ad1e26749a9d7b210dcc5851fa4b946c01
João Soares <jsoaresgeral@gmail.com>
2020-01-08 15:02:19 +0000

    Move reading functionality to separate static function
    Signed-off-by: João Soares <jsoaresgeral@gmail.com>

--- src/io/jaisoares/geo/Main.java
+++ src/io/jaisoares/geo/Main.java
@@ -1,31 +1,39 @@
 package io.jaisoares.geo;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		System.out.println("Enter coordinates x and y for point P (e.g. \"1, 3\")");
-		String pCoords = sc.nextLine();
-		int pX = Integer.parseInt(pCoords.split(",\\s*")[0]);
-		int pY = Integer.parseInt(pCoords.split(",\\s*")[1]);
-		Point p = new Point(pX, pY);
-
-		System.out.println("Enter coordinates x and y for point Q (e.g. \"2, 6\")");
-		String qCoords = sc.nextLine();
-		int qX = Integer.parseInt(qCoords.split(",\\s*")[0]);
-		int qY = Integer.parseInt(qCoords.split(",\\s*")[1]);
-		Point q = new Point(qX, qY);
-
-		// Close your scanner to avoid memory leaks and free resources
-		sc.close();
+		Point p = readPoint(br);
+		System.out.println("Enter coordinates x and y for point Q (e.g. \"1, 3\")");
+		Point q = readPoint(br);
 
 		Point r = p.add(q);
 		Point s = p.subtract(q);
 
 		System.out.println(p + " + " + q + " = " + r);
 		System.out.println(p + " - " + q + " = " + s);
+
+		br.close();
+	}
+
+	private static Point readPoint(BufferedReader br) throws IOException {
+		int x, y;
+		Point p = null;
+		try {
+			String[] coords = br.readLine().split(",\\s*");
+			x = Integer.parseInt(coords[0]);
+			y = Integer.parseInt(coords[1]);
+			p = new Point(x, y);
+		} catch (IOException ioe) {
+			System.out.println("Error reading your point coordinates" + ioe);
+		}
+		return p;
 	}
 }
