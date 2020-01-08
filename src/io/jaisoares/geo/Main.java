fff3d8713bb1e4e961433aa83e771a6c2bf49c6f
João Soares <jsoaresgeral@gmail.com>
2020-01-08 12:53:33 +0000

    Implement full point reader scanner
    Signed-off-by: João Soares <jsoaresgeral@gmail.com>

--- src/io/jaisoares/geo/Main.java
+++ src/io/jaisoares/geo/Main.java
@@ -7,21 +7,20 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter coordinate x for point P");
-		int pX = sc.nextInt();
-		System.out.println("Enter coordinate y for point P");
-		int pY = sc.nextInt();
+		System.out.println("Enter coordinates x and y for point P (e.g. \"1, 3\")");
+		String pCoords = sc.nextLine();
+		int pX = Integer.parseInt(pCoords.split(",\\s*")[0]);
+		int pY = Integer.parseInt(pCoords.split(",\\s*")[1]);
+		Point p = new Point(pX, pY);
 
-		System.out.println("Enter coordinate x for point Q");
-		int qX = sc.nextInt();
-		System.out.println("Enter coordinate y for point Q");
-		int qY = sc.nextInt();
+		System.out.println("Enter coordinates x and y for point Q (e.g. \"2, 6\")");
+		String qCoords = sc.nextLine();
+		int qX = Integer.parseInt(qCoords.split(",\\s*")[0]);
+		int qY = Integer.parseInt(qCoords.split(",\\s*")[1]);
+		Point q = new Point(qX, qY);
 
 		// Close your scanner to avoid memory leaks and free resources
 		sc.close();
-
-		Point p = new Point(pX, pY);
-		Point q = new Point(qX, qY);
 
 		Point r = p.add(q);
 		Point s = p.subtract(q);
